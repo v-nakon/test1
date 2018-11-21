6dde08198229a5a84c3a0382327642c05f558e15
v-nakon <nakonechny.vl@hotmail.com>
2018-11-22 01:59:47 +0200

    used addEventListener

--- scripts.js
+++ scripts.js
@@ -8,15 +8,14 @@ function nextSlide(){
     slides[currentSlide].className = 'slide showing';
 }
 
-$("body").on("mouseover", "slide", function(){
-  this.play();
-  $( this ).css( "background-color", "black" );
-});
 
-
-function getText() {
+function getText(event) {
     var input_text = document.getElementById("input_text").value;
     document.getElementById("result_text").innerHTML = input_text;
 	var e = document.getElementById("color_text");
 	result_text.style.color = e.options[e.selectedIndex].value;
+	event.preventDefault();
 }
+
+var el = document.getElementById("submit");
+el.addEventListener("click", getText, false);
